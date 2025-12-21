e37bdcea2e2152755f41bb65162d587ce919cb62
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2025-12-21 23:19:46 +0530

    feat: added server and db for backbone app

--- backbone-project/public/app.js
+++ backbone-project/public/app.js
@@ -11,7 +11,7 @@ var Blog = Backbone.Model.extend({
 // Backbone Collection
 
 var Blogs = Backbone.Collection.extend({
-  model: Blog,
+  url: 'http://localhost:3000/api/blogs'
 });
 
 // Create Collection Instance
@@ -52,12 +52,28 @@ var BlogView = Backbone.View.extend({
     this.$("#url").html("<input type='text' class='form-control' id='url-input-update' value='" + url + "'/>");
   },
   deleteBlog: function () {
-    this.model.destroy();
+    this.model.destroy({
+			success: function(response) {
+				console.log('Successfully DELETED blog with _id: ' + response.toJSON()._id);
+			},
+			error: function(err) {
+				console.log('Failed to delete blog!');
+			}
+		});
   },
   updateBlog: function () {
     this.model.set("author", this.$("#author-input-update").val());
     this.model.set("title", this.$("#title-input-update").val());
     this.model.set("url", this.$("#url-input-update").val());
+
+    this.model.save(null, {
+			success: function(response) {
+				console.log('Successfully UPDATED blog with _id: ' + response.toJSON()._id);
+			},
+			error: function(err) {
+				console.log('Failed to update blog!');
+			}
+		});
   },
   cancelBlog: function () {
     blogsView.render();
@@ -77,6 +93,17 @@ var BlogsView = Backbone.View.extend({
       }, 30)
     }, this);
     this.model.on("remove", this.render, this);
+
+    this.model.fetch({
+			success: function(response) {
+				_.each(response.toJSON(), function(item) {
+					console.log('Successfully GOT blog with _id: ' + item._id);
+				})
+			},
+			error: function() {
+				console.log('Failed to get blogs!');
+			}
+		});
   },
   render: function () {
     var self = this;
@@ -101,5 +128,13 @@ $(document).ready(function () {
     $("#title-input").val("");
     $("#url-input").val("");
     blogsCollection.add(blog);
+    blog.save(null, {
+			success: function(response) {
+				console.log('Successfully SAVED blog with _id: ' + response.toJSON()._id);
+			},
+			error: function() {
+				console.log('Failed to save blog!');
+			}
+		});
   });
 });
